f798fb4b0e80bbb8d8312b527be9ec8f02ae9947
liuxc123 <lxc_work@126.com>
2018-08-16 13:41:03 +0800

    提交

---
+++ Example/Pods/Target Support Files/Pods-GTMotionInterchange_Example/Pods-GTMotionInterchange_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GTMotionInterchange_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GTMotionInterchange_Example
+@end
---
+++ src/GTMotionInterchange.h
@@ -0,0 +1,12 @@
+//
+//  GTMotionInterchange.h
+//  Pods
+//
+//  Created by liuxc on 2018/8/14.
+//
+
+#ifndef GTMotionInterchange_h
+#define GTMotionInterchange_h
+
+
+#endif /* GTMotionInterchange_h */
